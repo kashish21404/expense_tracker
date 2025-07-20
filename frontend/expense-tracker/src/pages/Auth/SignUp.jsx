30b63790564fd39746c04cfa9b6e80d652426b81
Kashish21404 <kashishudaysuvarna@gmail.com>
2025-07-20 16:58:13 +0530

    signup api part, frontend+backend

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -5,6 +5,10 @@ import Input from "../../components/Inputs/Input";
 import { Link } from 'react-router-dom';
 import { validateEmail } from '../../utils/helper';
 import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/userContext';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUp=()=>{
     const [profilePic, setProfilePic]=useState(null);
@@ -13,7 +17,8 @@ const SignUp=()=>{
     const [password, setPassword]=useState("");
     const [error,setError]=useState(null);
     
-    const naviagte=useNavigate();
+    const {updateUser} =UserContext(UserContext);
+    const navigate=useNavigate();
     
     //Handles signup form submission
     const handleSignUp=async(e)=>{
@@ -42,6 +47,35 @@ const SignUp=()=>{
         setError("");
 
         //signup Api call
+        try{
+
+            //Upload image if present
+            if(profilePic){
+                const imgUploadRes=await uploadImage(profilePic)
+                profileImageUrl=imgUploadRes.imageUrl || "";
+            }
+
+            const response=await axiosInstance.post(API_PATHS.AUTH.REGISTER,{
+                fullName,
+                email,
+                password,
+                profileImageUrl
+            });
+
+            const {token,user}=response.data;
+
+            if(token){
+                localStorage.setItem("token",token);
+                updateUser(user);
+                navigate("/dashboard");
+            }
+        } catch(error){
+            if(error.response && error.response.data.message){
+                setError(error.response.data.message);
+            } else{
+                setError("Something went wrong.Please try again.");
+            }
+        }
     };
 
     return (
